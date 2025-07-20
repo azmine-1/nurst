b58849203be283eed365224f7a76c481c5d1266e
azmine-1 <sachchha222@gmail.com>
2025-07-19 23:52:05 -0400

    basic stackk pointer incrementt opcode

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,4 @@
-struct CPU {
+pub struct CPU {
     pc: u16,  // program counter 
     sp: u16, // stack pointer 
     ac: u8,  // accumulator 
@@ -6,15 +6,15 @@ struct CPU {
     y: u8,  //index register y
     status: u8, //status flags
 
-    memory[u8; 65536 ], //64 kb of memory 
+    memory: [u8; 65536 ], //64 kb of memory 
 }
 
 impl CPU {
-    fn new() -> self {
+    pub fn new() -> Self {
         CPU{
             pc: 0, 
             sp: 0xFF, 
-            a: 0, 
+            ac: 0, 
             x: 0, 
             y: 0, 
             status: 0, 
@@ -22,22 +22,22 @@ impl CPU {
         }
     }
 
-    fn reset(&mut self){
+    pub fn reset(&mut self){
         self.pc = 0;
         self.sp = 0xFF; 
-        self.a = 0; 
-        sel.x = 0;
+        self.ac = 0; 
+        self.x = 0;
         self.y = 0;
         self.status = 0;
     }
 
-    fn load(&mut self, program: [&u8], start_addr: usize){
+    pub fn load(&mut self, program: &[u8], start_addr: usize){
         self.memory[start_addr..start_addr + program.len()]
             .copy_from_slice(program);
         self.pc = start_addr as u16;
     }
 
-    fn fetch(&mut self) -> u8{
+    pub fn fetch(&mut self) -> u8{
         let opcode = self.memory[self.pc as usize]; 
         self.pc += 1;
         opcode
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod cpu;
+
+use cpu::CPU;
+
 fn main() {
-    println!("Hello, world!");
+    let mut cpu = CPU::new(); 
+
+    cpu.load(&[0xA9, 0x01, 0xA9, 0x95], 0x8000); 
+
+    println!("Firt opcode: 0x{:02x}", cpu.fetch()); 
+    println!("Second opcode: 0x{:02x}", cpu.fetch());
+    
 }
